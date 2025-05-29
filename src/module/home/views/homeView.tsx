e6778fc947838286dce6ca747110ae4a1342706e
Krish Koria <krishkoria2004@gmail.com>
2025-05-30 00:25:59 +0530

    feat: integrate TRPC for API handling; add query client and context management; implement hello query with Zod validation

--- src/module/home/views/homeView.tsx
+++ src/module/home/views/homeView.tsx
@@ -1,28 +1,14 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
+
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export default function HomeView() {
-  const { data: session } = authClient.useSession();
-  const router = useRouter();
-  if (!session) return <div>Loading....</div>;
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "Krish" }));
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">Welcome, {session.user.name}!</h1>
-      <p className="mt-4">You are already signed in.</p>
-      <Button
-        onClick={() => {
-          authClient.signOut({
-            fetchOptions: {
-              onSuccess: () => router.push("/sign-in"),
-            },
-          });
-        }}
-        className="mt-4"
-      >
-        Sign Out
-      </Button>
+    <div className="flex flex-col items-center justify-center p-4 gap-y-4">
+      <p>{data?.greeting}</p>
     </div>
   );
 }
