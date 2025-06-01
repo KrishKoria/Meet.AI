d191913112f88c3772262f4f46aa7e3f2a8bfe45
Krish Koria <krishkoria2004@gmail.com>
2025-06-01 14:19:25 +0530

    feat: enhance SpecificAgentView with avatar, badge, and instructions; update header with edit and delete options

--- src/module/agents/components/specificAgentViewHeader.tsx
+++ src/module/agents/components/specificAgentViewHeader.tsx
@@ -5,7 +5,19 @@ import {
   BreadcrumbList,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import { ChevronRightIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  ChevronRightIcon,
+  MoreVerticalIcon,
+  PencilIcon,
+  TrashIcon,
+} from "lucide-react";
 import Link from "next/link";
 
 interface SpecificAgentViewHeaderProps {
@@ -43,6 +55,24 @@ function SpecificAgentViewHeader({
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
+
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost">
+            <MoreVerticalIcon />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={onEdit}>
+            <PencilIcon className="text-black size-4" />
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={onRemove}>
+            <TrashIcon className="text-black size-4" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
--- src/module/agents/views/specificAgentView.tsx
+++ src/module/agents/views/specificAgentView.tsx
@@ -2,6 +2,9 @@
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import SpecificAgentViewHeader from "../components/specificAgentViewHeader";
+import { GenerateAvatar } from "@/components/generate-avatar";
+import { Badge } from "@/components/ui/badge";
+import { VideoIcon } from "lucide-react";
 
 interface SpecificAgentViewProps {
   agentId: string;
@@ -20,6 +23,30 @@ function SpecificAgentView({ agentId }: SpecificAgentViewProps) {
         onEdit={() => {}}
         onRemove={() => {}}
       />
+      <div className="bg-white rounded-lg border">
+        <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
+          <div className="flex items-center gap-x-3">
+            <GenerateAvatar
+              variant="botttsNeutral"
+              seed={data.name}
+              className="size-10"
+            />
+          </div>
+          <h2 className="text-2xl font-medium">{data.name}</h2>
+          <Badge
+            variant={"outline"}
+            className="flex items-center gap-x-2 [&>svg]:size-4"
+          >
+            <VideoIcon className="text-blue-700" />
+            {data.meetingCount}{" "}
+            {data.meetingCount === 1 ? "Meeting" : "Meetings"}
+          </Badge>
+          <div className="flex flex-col gap-y-4">
+            <p className="text-lg font-medium">Instructions</p>
+            <p className="text-neutral-800">{data.instructions}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
